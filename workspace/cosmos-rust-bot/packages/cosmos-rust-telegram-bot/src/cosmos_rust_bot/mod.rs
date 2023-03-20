ee2abef306169a359f3a181ee4d71aeec62728aa
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-03-20 10:41:18 +0700

    minor fix

--- workspace/cosmos-rust-bot/packages/cosmos-rust-telegram-bot/src/cosmos_rust_bot/mod.rs
+++ workspace/cosmos-rust-bot/packages/cosmos-rust-telegram-bot/src/cosmos_rust_bot/mod.rs
@@ -17,8 +17,7 @@ pub async fn handle_message(user_id: u64, message: String, db: &sled::Db) {
         msg.push_str(w);
     });
 
-    let msg_for_query = msg.replace(" subscribe","").replace(" unsubscribe","").replace("cosmos hub","cosmoshub");
-    let msg = msg.replace("cosmos hub","cosmoshub");
+    let msg_for_query = msg.replace(" subscribe","").replace(" unsubscribe",""); 
 
     let user_hash = UserMetaData::user_hash(user_id);
 
