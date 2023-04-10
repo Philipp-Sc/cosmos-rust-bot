24caca6bc4603208b88cbfc3553beb23323f1cfc
Philipp-Sc <philipp.schluetermann@udo.edu>
2023-04-10 16:36:15 +0700

    update

--- workspace/cosmos-rust-bot/packages/cosmos-rust-server/src/main.rs
+++ workspace/cosmos-rust-bot/packages/cosmos-rust-server/src/main.rs
@@ -1,5 +1,5 @@
 use actix_cors::Cors;
-use actix_web::{dev::ServiceRequest, get, App, Error, HttpServer, HttpResponse};
+use actix_web::{dev::ServiceRequest, get, App, Error, HttpServer, HttpResponse, web};
 use actix_web_httpauth::extractors::basic::{BasicAuth};
 use actix_web_httpauth::middleware::HttpAuthentication;
 use actix_web::middleware::Logger;
@@ -17,6 +17,7 @@ use reqwest::header::HeaderValue;
 use reqwest::header::CONTENT_TYPE;
 
 use actix_files::NamedFile;
+use actix_web::error::UrlGenerationError;
 use actix_web_httpauth::extractors::bearer::BearerAuth;
 use serde_json::json;
 
@@ -26,83 +27,80 @@ use cosmos_rust_interface::utils::entry::db::query::socket::client_send_query_re
 
 const QUERY_SOCKET: &str = "./tmp/cosmos_rust_bot_query_socket";
 
-/*
-async fn validator(
-    req: ServiceRequest,
-    bearer_auth: BearerAuth,
-) -> Result<ServiceRequest, (Error, ServiceRequest)> {
-    // the configured app_data config or a new config.
-    let config = req.app_data::<bearer::Config>()
-        .cloned()
-        .unwrap_or_default()
-        .realm("Restricted area")
-        .scope("cosmos-rust-server");
-
-    let request: UserQuery = UserQuery{ query_part: QueryPart::AuthQueryPart(AuthQueryPart{ token: bearer_auth.token().parse::<u64>().unwrap(), user_hash: 0u64 }), settings_part: SettingsPart {
-        subscribe: None,
-        unsubscribe: None,
-        register: None,
-        user_hash: Some(0u64)
-    } };
-    let response = client_send_query_request(QUERY_SOCKET,request).unwrap();
-    if bearer_auth.token() == "0" || req.path().starts_with("/static") {
-        Ok(req)
-    } else {
-        Err((AuthenticationError::from(config).into(), req))
-    }
-}*/
 
 async fn validator(
     req: ServiceRequest,
     credentials: BasicAuth,
 ) -> Result<ServiceRequest, (Error, ServiceRequest)> {
 
-    if let (Some(Ok(token)),Ok(user_hash)) = (credentials.password().map(|pw| pw.parse::<u64>()),credentials.user_id().parse::<u64>()){
-        let request: UserQuery = UserQuery{ query_part: QueryPart::AuthQueryPart(AuthQueryPart{ token, user_hash }), settings_part: SettingsPart {
-            subscribe: None,
-            unsubscribe: None,
-            register: None,
-            user_hash: Some(user_hash)
-        } };
+    // Check if the request matches the file route
+    let is_file_request = req.path().starts_with("/static/");
 
-        if let Ok(CosmosRustServerValue::Notification(n)) = client_send_query_request(QUERY_SOCKET,request) {
+    // If the request is a file request, skip the authentication check
+    if is_file_request {
+        Ok(req)
+    }else {
+    // For any other request the user needs to pass the authentication check
+        if let (Some(Ok(token)), Ok(user_hash)) = (credentials.password().map(|pw| pw.parse::<u64>()), credentials.user_id().parse::<u64>()) {
+            let request: UserQuery = UserQuery {
+                query_part: QueryPart::AuthQueryPart(AuthQueryPart { token, user_hash }),
+                settings_part: SettingsPart {
+                    subscribe: None,
+                    unsubscribe: None,
+                    register: None,
+                    user_hash: Some(user_hash)
+                }
+            };
 
-            println!("Notification: {:?}", &n);
+            if let Ok(CosmosRustServerValue::Notification(n)) = client_send_query_request(QUERY_SOCKET, request) {
+                log::info!("Notification: {:?}", &n);
 
-            let mut is_authorized = false;
+                let mut is_authorized = false;
 
-            match n.query.query_part {
-                QueryPart::AuthQueryPart(_) => {
-                    for i in 0..n.entries.len() {
-                        match &n.entries[i] {
-                            CosmosRustBotValue::Authorization(auth) => {
-                                is_authorized = auth.is_authorized;
+                match n.query.query_part {
+                    QueryPart::AuthQueryPart(_) => {
+                        for i in 0..n.entries.len() {
+                            match &n.entries[i] {
+                                CosmosRustBotValue::Authorization(auth) => {
+                                    is_authorized = auth.is_authorized;
+                                }
+                                _ => {
+                                    is_authorized = false;
+                                }
                             }
-                            _ => {
-                                is_authorized = false;
-                            }
+                            break;
                         }
-                        break;
+                    },
+                    _ => {
+                        is_authorized = false;
                     }
-                },
-                _ => {
-                    is_authorized = false;
+                }
+
+                if is_authorized {
+                    return Ok(req);
                 }
             }
-
-            if is_authorized {
-                return Ok(req);
-            }
         }
+        Err((Error::from(AuthenticationError::new(Basic::default())), req))
     }
-    Err((Error::from(AuthenticationError::new(Basic::default())), req))
 }
 
 
-#[get("/verify")]
 async fn verify(_req: HttpRequest, _basic_auth: BasicAuth) -> actix_web::Result<HttpResponse> {
     Ok(HttpResponse::Ok().finish())
 }
+
+async fn translate(_req: HttpRequest, info: web::Path<(String, String, String)>) -> actix_web::Result<HttpResponse> {
+    let (lang, blockchain, id) = info.into_inner();
+
+    // where is the dirty work done?
+
+    // Now I just need to ask Cosmos-Rust-Bot to fetch a translation. CRB may or may not need to query GPT.
+    // On Okay, a json file is returned, that by then should now exist.
+
+    Ok(HttpResponse::Ok().finish())
+}
+
 
 /*
 #[get("/subscriptions")]
@@ -135,33 +133,41 @@ async fn file_exists(req: HttpRequest) -> actix_web::Result<HttpResponse> {
 }
 */
 
-/*
-#[get("/static/{filename:.*}")]
-async fn file(req: HttpRequest, _bearer_auth: BearerAuth) -> actix_web::Result<NamedFile> {
-    Ok(NamedFile::open(format!("./static/{}",req.match_info().query("filename")))?)
-}*/
+async fn file(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let filepath = req.match_info().query("filepath");
+    let ext = req.match_info().query("ext");
+
+    // Check if the file extension is allowed
+    if ext == "json" || ext == "html" {
+        Ok(NamedFile::open(format!("./tmp/public/{}.{}", filepath, ext))?)
+    } else {
+        Err(UrlGenerationError::ResourceNotFound.into())
+    }
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
+        let auth = HttpAuthentication::basic(validator);
         App::new()
-            /*.app_data(
-                bearer::Config::default()
-                    .realm("Restricted area")
-                    .scope("cosmos-rust-server"),
-            )
-            .wrap(HttpAuthentication::bearer(validator))
-            */
-            .wrap(Logger::default())
-            .wrap(HttpAuthentication::basic(validator))
-            // ensure the CORS middleware is wrapped around the httpauth middleware so it is able to
-            // add headers to error responses
+            //.wrap(auth) // to enable auth for all services
             .wrap(Cors::permissive())
-            //.service(file_exists)
-            .service(verify)
-        //.service(subscriptions)
-        //.service(file)
-    })
+            .service(
+                web::resource("/verify")
+                    .wrap(auth.clone())
+                    .route(web::get().to(verify)),
+            )
+            .service(
+                web::resource("/translate/{lang}/{blockchain}/{id}")
+                    //.wrap(auth)
+                    .route(web::get().to(translate)),
+            )
+            .service(
+                web::resource("/static/{filepath:.*}.{ext:html|json}")
+                    //.wrap(auth)
+                    .route(web::get().to(file)),
+            )
+        })
         .bind(("0.0.0.0", 8081))?
         .run()
         .await
